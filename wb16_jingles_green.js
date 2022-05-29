c49773d3fc2494778496d7493fc49e86bade437a
dudu5988 <106456935+dudu5988@users.noreply.github.com>
2022-05-28 23:15:30 -0300

    commit changes
    meu primeiro

---
+++ wb16_jingles_green.js
@@ -0,0 +1,13 @@
+   
+   $(document).ready(function()
+   {
+      $('#headerMenu .dropdown-toggle').dropdown({popperConfig:{placement:'bottom-start',modifiers:{computeStyle:{gpuAcceleration:false}}}});
+      $(document).on('click','.headerMenu-navbar-collapse.show',function(e)
+      {
+         if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) 
+         {
+            $(this).collapse('hide');
+         }
+      });
+      $("#Carousel1").bootstrapcarousel({interval:3000, pause: false});
+   });
